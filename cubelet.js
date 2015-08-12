3d768096d9cd2f3ed5fe849594210caddbf06268
Donald Ness <donald.ness@gmail.com>
2015-08-12 15:01:37 -0600

    Consistently use BlockType vs Type

--- cubelet.js
+++ cubelet.js
@@ -1,17 +1,17 @@
-var Types = require('./config.json')['blockTypes']
+var BlockTypes = require('./config.json')['blockTypes']
 var __ = require('underscore')
 
 var Cubelet = function (blockId, hopCount, blockType) {
   this.blockId = blockId
   this.hopCount = hopCount
-  this.blockType = blockType || Types.UNKNOWN
+  this.blockType = blockType || BlockTypes.UNKNOWN
 }
 
 module.exports = Cubelet
-module.exports.BlockTypes = Types
+module.exports.BlockTypes = BlockTypes
 
 module.exports.typeForTypeId = function (typeId) {
-  return __(Types).find(function (type) {
+  return __(BlockTypes).find(function (type) {
     return type.typeId === typeId
-  }) || Types.UNKNOWN
+  }) || BlockTypes.UNKNOWN
 }
