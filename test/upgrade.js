e04fc5b2c7bc1f84685163b305d3bae6a3d51438
Eric Lundby <Eric.Lundby@modrobotics.com>
2015-08-18 16:42:10 -0600

    Add upgrade jump tests: discovery->OS4->discovery->OS3->discovery

--- test/upgrade.js
+++ test/upgrade.js
@@ -3,6 +3,8 @@ var config = require('./config');
 var cubelets = require('../index');
 var Upgrade = require('../upgrade');
 var UpgradeProtocol = require('../protocol/bootstrap/upgrade');
+var ImagoProtocol = require('../protocol/imago');
+var ClassicProtocol = require('../protocol/classic');
 var __ = require('underscore');
 
 var client = cubelets.connect(config.device, function (err) {
@@ -25,12 +27,44 @@ var client = cubelets.connect(config.device, function (err) {
       });*/
 
       test('jump to os4 mode', function (t) {
+      	t.plan(2);
         client.setProtocol(UpgradeProtocol);
         client.sendRequest(new UpgradeProtocol.messages.SetBootstrapModeRequest(1), function (err, response) {
           t.ifError(err);
           t.equals(response.mode, 1);
         });
       });
+      
+      test('jump to discovery mode from OS4', function (t) {
+      	t.plan(1);
+        client.setProtocol(ImagoProtocol);
+        client.sendCommand(new ImagoProtocol.messages.ResetCommand());
+        setTimeout(function(){
+        	t.pass("Sent reset command");
+        }, 500);
+      });
+      
+      //TODO: make sure we are getting discovery mode packets
+      
+      test('jump to os3 mode', function (t) {
+      	t.plan(2);
+        client.setProtocol(UpgradeProtocol);
+        client.sendRequest(new UpgradeProtocol.messages.SetBootstrapModeRequest(0), function (err, response) {
+          t.ifError(err);
+          t.equals(response.mode, 0);
+        });
+      });
+      
+      test('jump to discovery mode from OS3', function (t) {
+      	t.plan(1);
+        client.setProtocol(ClassicProtocol);
+        client.sendCommand(new ClassicProtocol.messages.ResetCommand());
+        setTimeout(function(){
+        	t.pass("Sent reset command");
+        }, 500);
+      });
+      
+      //TODO: make sure we are getting discovery mode packets      
 
       test('disconnect', function (t) {
         t.plan(1);
