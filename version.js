2cf12c3109a5b8850bc96c204255dc681832c3a5
Donald Ness <donald.ness@gmail.com>
2015-08-25 23:10:19 -0600

    standard --format version.js

--- version.js
+++ version.js
@@ -1,44 +1,30 @@
-module.exports = function Version(major, minor, patch) {
+module.exports = function Version (major, minor, patch) {
   this.major = major || 0
   this.minor = minor || 0
   this.patch = patch || 0
 
   this.isEqual = function (v) {
     if (arguments.length === 1) {
-      return (
-        v.major === this.major &&
-        v.minor === this.minor &&
-        v.patch === this.patch
-      )
+      return v.major === this.major && v.minor === this.minor && v.patch === this.patch
     } else if (arguments.length === 3) {
-      return (
-        arguments[0] === this.major &&
-        arguments[1] === this.minor &&
-        arguments[2] === this.patch
-      )
+      return arguments[0] === this.major && arguments[1] === this.minor && arguments[2] === this.patch
     } else {
       return false
     }
   }
 
   this.isGreaterThan = function (v) {
-  	if(this.isEqual(v))
-  	{
-  		return false;
-  	}
-  	else if(this.major > v.major)
-  	{
-  		return true;
-  	}
-  	else if(this.major == v.major && this.minor > v.minor)
-  	{
-  		return true;
-  	}
-  	else if(this.major == v.major && this.minor == v.minor && this.patch > v.patch)
-  	{
-  		return true;
-  	}
-  	return false;
+    if (this.isEqual(v)) {
+      return false
+    } else if (this.major > v.major) {
+      return true
+    } else if (this.major === v.major && this.minor > v.minor) {
+      return true
+    } else if (this.major === v.major && this.minor === v.minor && this.patch > v.patch) {
+      return true
+    } else {
+      return false
+    }
   }
 
   this.isGreaterThanOrEqual = function (v) {
@@ -46,23 +32,17 @@ module.exports = function Version(major, minor, patch) {
   }
 
   this.isLessThan = function (v) {
-  	if(this.isEqual(v))
-  	{
-  		return false;
-  	}
-  	else if(this.major < v.major)
-  	{
-  		return true;
-  	}
-  	else if(this.major == v.major && this.minor < v.minor)
-  	{
-  		return true;
-  	}
-  	else if(this.major == v.major && this.minor == v.minor && this.patch < v.patch)
-  	{
-  		return true;
-  	}
-  	return false;
+    if (this.isEqual(v)) {
+      return false
+    } else if (this.major < v.major) {
+      return true
+    } else if (this.major === v.major && this.minor < v.minor) {
+      return true
+    } else if (this.major === v.major && this.minor === v.minor && this.patch < v.patch) {
+      return true
+    } else {
+      return false
+    }
   }
 
   this.isLessThanOrEqual = function (v) {
