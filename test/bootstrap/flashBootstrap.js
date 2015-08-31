fb93d0e5bebac7c4046e977d5aae3871136b2afa
Donald Ness <donald.ness@gmail.com>
2015-08-31 14:53:09 -0600

    Remove detection steps from flash scripts. Use utils for this.

--- test/bootstrap/flashBootstrap.js
+++ test/bootstrap/flashBootstrap.js
@@ -5,16 +5,12 @@ var cubelets = require('../../index')
 var Block = require('../../block')
 var BlockTypes = require('../../blockTypes')
 var MCUTypes = require('../../mcuTypes')
-var Upgrade = require('../../upgrade')
 var ClassicProtocol = require('../../protocol/classic')
-var Flash = ClassicProtocol.Flash
-var Program = ClassicProtocol.Program
+var ClassicFlash = ClassicProtocol.Flash
+var ClassicProgram = ClassicProtocol.Program
 
 var bluetoothBlockId = config.map.type.bluetooth
 
-// Note: block should start this test with 
-// classic firwmare running. It will upgrade the block
-// to the bootstrap firmware.
 var client = cubelets.connect(config.device, function (err) {
   test('connected', function (t) {
     t.plan(1)
@@ -22,17 +18,6 @@ var client = cubelets.connect(config.device, function (err) {
       t.end(err)
     } else {
       t.pass('connected')
-
-      var upgrade = new Upgrade(client)
-
-      test('must detect classic firmware', function (t) {
-        t.plan(3)
-        upgrade.detectIfNeeded(function (err, needsUpgrade, firmwareType) {
-          t.ifError(err, 'no err')
-          t.ok(needsUpgrade, 'needs upgrade')
-          t.equal(firmwareType, 0, 'has classic firmware')
-        })
-      })
 
       test('set classic protocol', function (t) {
         t.plan(1)
@@ -43,11 +28,11 @@ var client = cubelets.connect(config.device, function (err) {
       test('flash bluetooth bootstrap firmware', function (t) {
         t.plan(2)
         var hex = fs.readFileSync('./upgrade/hex/bluetooth_bootstrap.hex')
-        var program = new Program(hex)
+        var program = new ClassicProgram(hex)
         t.ok(program.valid, 'firmware valid')
         var block = new Block(bluetoothBlockId, 0, BlockTypes.BLUETOOTH)
         block._mcuType = MCUTypes.AVR
-        var flash = new Flash(client, {
+        var flash = new ClassicFlash(client, {
           skipSafeCheck: true
         })
         flash.programToBlock(program, block, function (err) {
@@ -58,7 +43,7 @@ var client = cubelets.connect(config.device, function (err) {
         })
       })
 
-      test.skip('disconnect', function (t) {
+      test('disconnect', function (t) {
         t.plan(1)
         client.disconnect(t.ifError)
       })
