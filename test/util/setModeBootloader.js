cdf697189fc1a30b32243ccaa27532e89397be70
Donald Ness <donald.ness@gmail.com>
2015-08-31 14:52:18 -0600

    Add support for jumping to bootloader from any of the 3 modes (classic, imago, bootstrap)

--- test/util/setModeBootloader.js
+++ test/util/setModeBootloader.js
@@ -1,6 +1,8 @@
 var test = require('tape')
 var config = require('../config')
 var cubelets = require('../../index')
+var BootstrapProtocol = require('../../protocol/bootstrap')
+var ClassicProtocol = require('../../protocol/classic')
 var ImagoProtocol = require('../../protocol/imago')
 var __ = require('underscore')
 
@@ -12,8 +14,28 @@ var client = cubelets.connect(config.device, function (err) {
     } else {
       t.pass('connected')
 
-      test('jump to bootloader', function (t) {
+      test('jump to bootloader from bootstrap', function (t) {
         t.plan(2)
+        client.setProtocol(BootstrapProtocol)
+        var req = new BootstrapProtocol.messages.SetBootstrapModeRequest(0)
+        client.sendRequest(req, function (err, res) {
+          t.ifError(err)
+          t.equal(res.mode, 0, 'jumped to classic')
+        })
+      })
+
+      test.skip('jump to bootloader from classic', function (t) {
+        t.plan(1)
+        client.sendData(new Buffer([
+          'L'.charCodeAt(0)
+        ]), function (err) {
+          t.ifError(err)
+        })
+      })
+
+      test.skip('jump to bootloader from imago', function (t) {
+        t.plan(2)
+        client.setProtocol(ImagoProtocol)
         var req = new ImagoProtocol.messages.SetModeRequest(0)
         client.sendRequest(req, function (err, res) {
           t.ifError(err)
