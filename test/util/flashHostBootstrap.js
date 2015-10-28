f99bc27c9d3cd4f14d0a15929c8ca9bb3ff2393b
Eric Lundby <Eric.Lundby@modrobotics.com>
2015-10-28 11:44:27 -0600

    Add determine BT ID to the flashHostBootstrap util

--- test/util/flashHostBootstrap.js
+++ test/util/flashHostBootstrap.js
@@ -24,6 +24,20 @@ var client = cubelets.connect(config.device, function (err) {
         client.setProtocol(ClassicProtocol)
         t.pass('set protocol')
       })
+      
+      test('fetch host id', function(t){
+      	t.plan(3)
+      	// Determine Host ID
+  			var req = new ClassicProtocol.messages.GetNeighborBlocksRequest()
+  			client.sendRequest(req, function (err, res) {
+  				t.ifError(err, 'fetch id err')
+  				var originBlockId = res.originBlockId
+  				
+  				t.ok(originBlockId != 0, 'bt id valid')
+  				bluetoothBlockId = originBlockId
+  				t.pass("found bt id")
+  			})      	
+      })
 
       test('flash bluetooth bootstrap firmware', function (t) {
         t.plan(2)
@@ -33,8 +47,7 @@ var client = cubelets.connect(config.device, function (err) {
         var block = new Block(bluetoothBlockId, 0, BlockTypes.BLUETOOTH)
         block._mcuType = MCUTypes.AVR
         var flash = new ClassicFlash(client, {
-          skipSafeCheck: true,
-          skipReadyCommand: true
+          skipSafeCheck: true
         })
         flash.programToBlock(program, block, function (err) {
           t.ifError(err, 'flash err')
