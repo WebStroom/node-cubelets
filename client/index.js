dc669cabbd636e662610d9dfec5781cb818eacd0
Eric Lundby <Eric.Lundby@modrobotics.com>
2016-02-15 10:34:11 -0700

    Use platform dependent client.

--- client/index.js
+++ client/index.js
@@ -1,4 +1,14 @@
-module.exports = require('./bluetoothSerial')
+var platform = process.platform;
+platform = /^win/.test(platform)? 'win' : /^darwin/.test(platform)? 'mac' : 'linux' + (process.arch == 'ia32' ? '32' : '64');
+if(platform === 'win')
+{
+	module.exports = require('./bluetoothSerial')
+}
+else
+{//Use regular serial port on OSX
+	module.exports = require('./serial')
+}
+
 //module.exports = require('./serial')
 //module.exports = require('./net)
 //module.exports = require('./ws)
